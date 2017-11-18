7cbde1182c9db027a573d28d7557a575be12ac8c
Ciccio Ingrassia <ciccio.ingrassia111@outlook.com>
2017-11-18 08:52:50 +0100

    Layout basic project structure

---
+++ src/main/java/com/lucapruneti/mimosapudica/MimosaPudicaApplication.java
@@ -0,0 +1,12 @@
+package com.lucapruneti.mimosapudica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimosaPudicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MimosaPudicaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lucapruneti/mimosapudica/MimosaPudicaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lucapruneti.mimosapudica;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MimosaPudicaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
